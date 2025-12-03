053ef55bf00f49cc8e52f089fc929cb36315e0b5
Your Name <you@example.com>
2025-12-03 08:39:22 +0000

    docker mysq

--- replica/scripts/check_mysql.sh
+++ replica/scripts/check_mysql.sh
@@ -1,8 +1,16 @@
-
 #!/bin/bash
+
+LOG="/home/sder/ha/replica/logs/keepalived-mysql.log"
+echo "$(date): [NOTIFY] Checking MySQL..." >> $LOG
+
+MYSQL_USER="root"
+MYSQL_PASS="s<9!Own1z4"
+
 # 检查MySQL服务是否可用的简单脚本
-if mysql -uroot -p's<9!Own1z4' -h 127.0.0.1 -e "SELECT 1;" &> /dev/null; then
+if docker exec -it mysql -u$MYSQL_USER -p$MYSQL_PASS -h 127.0.0.1 -e "SELECT 1;" &> /dev/null; then
+    echo "$(date): [NOTIFY] MySQL is available." >> $LOG
     exit 0
 else
+    echo "$(date): [ERROR] MySQL is not available." >> $LOG
     exit 1
 fi
